347858e75e3b872c6325c5571e621e513b8a57b3
Bhoomika Rao <raobhoomika11@gmail.com>
2026-02-18 04:42:12 +0530

    Refactored UC3 to use String Array and Loop for UC4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,22 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        // Use String.join(" ", ...) to join the parts of each line with spaces. [cite: 64]
-        // This is more efficient than the + operator for building patterns. [cite: 33]
+        // 1. Define a String array with a size equal to the number of banner lines [cite: 98]
+        String[] lines = new String[7];
 
-        System.out.println(String.join(" ", "   *** ", "   *** ", "****** ", " ***** "));
-        System.out.println(String.join(" ", "  ** ** ", "  ** ** ", "** ** ", " ** ** "));
-        System.out.println(String.join(" ", " ** ** ", " ** ** ", "** ** ", " ** "));
-        System.out.println(String.join(" ", " ** ** ", " ** ** ", "****** ", " ***** "));
-        System.out.println(String.join(" ", " ** ** ", " ** ** ", "** ", "      ** "));
-        System.out.println(String.join(" ", "  ** ** ", "  ** ** ", "** ", " ** ** "));
-        System.out.println(String.join(" ", "   *** ", "   *** ", "** ", " ***** "));
+        // 2. Populate each index of the array using String.join() [cite: 81, 98]
+        lines[0] = String.join(" ", "   *** ", "   *** ", "****** ", " ***** ");
+        lines[1] = String.join(" ", "  ** ** ", "  ** ** ", "** ** ", " ** ** ");
+        lines[2] = String.join(" ", " ** ** ", " ** ** ", "** ** ", " ** ");
+        lines[3] = String.join(" ", " ** ** ", " ** ** ", "****** ", " ***** ");
+        lines[4] = String.join(" ", " ** ** ", " ** ** ", "** ", "      ** ");
+        lines[5] = String.join(" ", "  ** ** ", "  ** ** ", "** ", " ** ** ");
+        lines[6] = String.join(" ", "   *** ", "   *** ", "** ", " ***** ");
+
+        // 3. Use an enhanced for-each loop to print each line sequentially [cite: 83, 84, 98]
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
+
